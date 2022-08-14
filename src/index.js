c25c822650f4bb7364b524758225c6283a991a2b
Ben Hanson <ben.hanson1991@googlemail.com>
2022-08-14 17:49:55 +0100

    update

--- src/index.js
+++ src/index.js
@@ -2,15 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Homepage from "./pages/Homepage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
