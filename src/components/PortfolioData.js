f2cbf17a7e6e35b96863ba7c1787ef46aadcb81b
Ben Hanson <ben.hanson1991@googlemail.com>
2022-08-14 17:02:41 +0100

    Initial Commit

---
+++ src/components/PortfolioData.js
@@ -0,0 +1,20 @@
+const PortfolioData = [
+  {
+    id: 1,
+    src: "./assets/digital-clock.PNG",
+    alt: "go to Digital Clock",
+    linksource: "https://calm-basbousa-d80a2f.netlify.app/",
+    heading: "Digital Clock",
+    text: "Built using React.js & CSS3",
+  },
+  // {
+  //   id: 2,
+  //   src: "./assets/battery-indicator.PNG",
+  //   alt: "go to Battery Level Indicator",
+  //   linksource: "/battery-level-indicator/build/index.html",
+  //   heading: "Battery Level Indicator",
+  //   text: "Built using React.js & CSS3",
+  // },
+];
+
+export default PortfolioData;
