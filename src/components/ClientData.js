d16e23a9492eeb09819557177d299db536037882
Ben Hanson <ben.hanson1991@googlemail.com>
2022-12-03 20:57:06 +0000

    ItemCardUpdates

--- src/components/ClientData.js
+++ src/components/ClientData.js
@@ -1,25 +1,25 @@
 const ClientData = [
+  // {
+  //   id: 1,
+  //   src: "./assets/whittard.png",
+  //   alt: "Whittard of Chelsea logo",
+  //   heading: "Whittard of Chelsea",
+  //   startDate: new Date("2021-10-25"),
+  //   endDate: new Date("2022-09-25"),
+  //   responsibilities: ["another test","double test"],
+  //   technologies: ["Test1", "Test2"],
+  // },
   {
-    id: 1,
+    id: 2,
     src: "./assets/whittard.png",
     alt: "Whittard of Chelsea logo",
     heading: "Whittard of Chelsea",
-    text: "A premium hot beverage retailer, based in Oxfordshire, I was responsible for the technical implementation of marketing assets, new features and core web vital improvements. I Optimised code for performance, improving the Lighthouse performance score from 16 to 48 within a six month period. Improved the average page load speed from 18 seconds to 8, which resulted in a 10% reduction in the average user bounce rate over a six month period.",
-  },
-  {
-    id: 2,
-    src: "./assets/waitrose.png",
-    alt: "Waitrose & Partners logo",
-    heading: "Waitrose & Partners",
-    text: "One of Britains most iconic supermarkets, I was employed as a Senior Data Analyst working closely with category managers to analyse customer shopping patterns. Developed strong analytical, problem solving skills. Enhanced my coding knowledge of SQL and R Studio.",
-  },
-  {
-    id: 3,
-    src: "./assets/Allianz.png",
-    alt: "Allianz Insurance logo",
-    heading: "Allianz Insurance",
-    text: "A global insurance company, I was employed as a Portfolio Analyst, analysing claims data and managing their UK Home & Motor portfolios. Developed strong analytical, problem solving skills using SQL/SAS software.",
+    startDate: new Date("2021-10-25"),
+    endDate: new Date("2022-09-25"),
+    responsibilities: ["Technical implementation of marketing assets", "Core Web Vital improvment", "Migration "],
+    technologies: ["HTML", "CSS3/TailwindCSS","Javascript","GitHub","Salesforce eCommerce Cloud"],
   },
 ];
 
 export default ClientData;
+    // text: "A premium hot beveragx  e retailer, based in Oxfordshire, I was responsible for the technical implementation of marketing assets, new features and core web vital improvements. I Optimised code for performance, improving the Lighthouse performance score from 16 to 48 within a six month period. Improved the average page load speed from 18 seconds to 8, which resulted in a 10% reduction in the average user bounce rate over a six month period.",
